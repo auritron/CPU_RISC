11a94fb22a2aec6e18cea683972829443b1e54f6
auritron <myself.anath@gmail.com>
2024-09-27 18:37:31 +0000

    readded files

---
+++ RISC_CPU_python.py
@@ -0,0 +1,34 @@
+import json
+
+instructions = {1:"LOAD", #LOAD from RAM to Cache
+                2:"SEND", #SEND from Cache to RAM
+                3:"SWAP", #SWAP the data in two Registers
+                4:"ADD",  #ADD the values of two Registers
+                5:"SUB",  #SUBTRACT the values of two Registers
+                6:"MUL",  #MULTIPLY the values of two Registers
+                7:"DIV",  #DIVIDE the values of two Registers
+                8:"NOT",  #BITWISE NOT
+                9:"AND",  #BITWISE AND
+                10:"OR",  #BITWISE OR
+                11:"XOR", #BITWISE XOR
+                12:"SET", #SET VALUE of bit in a register to 0 or 1 
+                13:"SETR" #SET Multiple VALUES in a range of bits in a single register to 0 or 1
+                } 
+
+cache = {
+        1:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        2:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        3:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        4:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        5:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        6:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        7:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        8:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        }
+
+flags = {
+        "zero":0,  #Zero Flag
+        "ngtve":0, #Negative flag
+        "carry":0, #Carry flag
+        "ovflw":0  #Overflow Flag
+        }
