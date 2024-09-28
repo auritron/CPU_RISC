50ed6102922e50629777f30bda40ab321932e562
auritron <myself.anath@gmail.com>
2024-09-28 19:17:49 +0000

    instructions list desc and new instructions class

---
+++ Instruction_process.md
@@ -0,0 +1,10 @@
+Instructions -
+('>' represents a bullet point, '-' represents pseudocode)
+
+1. LOAD -
+> Locate memory address from RAM
+> Copy memory from RAM to Cache object
+- 
+
+2. SEND -
+> 
--- RISC_CPU_python.py
+++ RISC_CPU_python.py
@@ -1,31 +1,37 @@
 import json
 
 class CPU:
+        pass
 
-        instructions = {
-                1:"LOAD",   #LOAD from RAM to Cache
-                2:"SEND",   #SEND from Cache to RAM
-                3:"SWAP",   #SWAP the data in two Registers
-                4:"ADD",    #ADD the values of two Registers
-                5:"SUB",    #SUBTRACT the values of two Registers
-                6:"MUL",    #MULTIPLY the values of two Registers
-                7:"DIV",    #DIVIDE the values of two Registers
-                8:"NOT",    #BITWISE NOT
-                9:"AND",    #BITWISE AND
-                10:"OR",    #BITWISE OR
-                11:"XOR",   #BITWISE XOR
-                12:"SET",   #SET VALUE of bit in a register to 0 or 1 
-                13:"SETR",  #SET Multiple VALUES in a range of bits in a single register to 0 or 1
-                14:"STL",   #SHIFT bits LEFT
-                15:"STR",   #SHIFT bits RIGHT
-                16:"RTL",   #ROTATE bits LEFT
-                17:"RTR",   #ROTATE bist RIGHT
-                18:"CMP",   #COMPARE two registers
-                19:"GOTO",  #GO TO LABEL unconditionally
-                20:"WEQ",   #GO TO LABEL if f(Z) = 1
-                21:"WGT",   #GO TO LABEl if f(Z) = 0 and f(S) = 0
-                22:"WLT"
-                } 
+class Instructions:
+
+        def __init__(self) -> None:
+
+                #instructions list
+                self.instructions = {
+                        1:"LOAD",   #LOAD from RAM to Cache
+                        2:"SEND",   #SEND from Cache to RAM
+                        3:"SWAP",   #SWAP the data in two Registers
+                        4:"ADD",    #ADD the values of two Registers
+                        5:"SUB",    #SUBTRACT the values of two Registers
+                        6:"MUL",    #MULTIPLY the values of two Registers
+                        7:"DIV",    #DIVIDE the values of two Registers
+                        8:"NOT",    #BITWISE NOT
+                        9:"AND",    #BITWISE AND
+                        10:"OR",    #BITWISE OR
+                        11:"XOR",   #BITWISE XOR
+                        12:"SET",   #SET VALUE of bit in a register to 0 or 1 
+                        13:"SETR",  #SET Multiple VALUES in a range of bits in a single register to 0 or 1
+                        14:"STL",   #SHIFT bits LEFT
+                        15:"STR",   #SHIFT bits RIGHT
+                        16:"RTL",   #ROTATE bits LEFT
+                        17:"RTR",   #ROTATE bist RIGHT
+                        18:"CMP",   #COMPARE two registers
+                        19:"GOTO",  #GO TO LABEL unconditionally
+                        20:"WEQ",   #GO TO LABEL if f(Z) = 1
+                        21:"WGT",   #GO TO LABEl if f(Z) = 0 and f(S) = 0
+                        22:"WLT"
+                }                  
 
 class Memory:
 
